764fde62dabb8be05977db1a8356181c8685dd6a
bill <milias.vasilis@gmail.com>
2017-05-13 16:17:13 +0200

    plot per category added

--- Assignment_1/src/fraud_analysis.py
+++ Assignment_1/src/fraud_analysis.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 
 data_path = '../datasets/data_for_student_case.csv'
@@ -35,19 +36,37 @@ class Fraud:
         #result.to_csv(path=path,index_label=["issuercountrycode","transaction_count"],index=True)
         return result
 
+    def total_per_cardid(self):
+        result = self.df["card_id"].value_counts().to_dict()
+        return result
+
+
     # Output: dataframe that contains only the "Chargebacks"
     def filter_chargeback_records(self):
         result = self.df.loc[self.df["simple_journal"] == "Chargeback"]
         return result
 
     # Output: dataframe that contains only the "Settled"
-    def filter_settled_records(self):
-        result = self.df.loc[self.df["simple_journal"] == "Settled"]
+    def filter_records(self, category):
+        result = self.df.loc[self.df["simple_journal"] == category]
         return result
 
     def filter_refused_records(self):
         result = self.df.loc[self.df["simple_journal"] == "Refused"]
         return result
+
+    def plot_data_balance(self):
+        objects = tuple(self.df["simple_journal"].unique())
+        y_pos = np.arange(len(objects))
+        num_chargeback = self.df.loc[self.df["simple_journal"] == "Chargeback"].shape[0]
+        num_refused = self.df.loc[self.df["simple_journal"] == "Refused"].shape[0]
+        num_settled = (self.df.loc[self.df["simple_journal"] == "Settled"]).shape[0]
+        number_of_records = [num_chargeback, num_refused, num_settled]
+        plt.bar(y_pos, number_of_records, align='center', alpha=0.4)
+        plt.xticks(y_pos, objects)
+        plt.ylabel('Number of Transactions`')
+        plt.title('Balance of the Data')
+        plt.show()
 
     @staticmethod
     def string_to_timestamp(date_string):  # convert time string to float value
@@ -55,28 +74,30 @@ class Fraud:
         return time.mktime(time_stamp)
 
 
-
 # Initialization of dataframe object
 fraud_obj = Fraud()
 print(fraud_obj.df.shape)
+
 # print fraud per country
 #print(fraud_obj.total_per_country())
+
+# print fraud per card id
+cardid_dict = fraud_obj.total_per_cardid()
+plt.bar(list(cardid_dict.keys()), cardid_dict.values(), width=1.0, color='g')
+
 
 # print the different values of 'simple_journal'
 print(fraud_obj.df["simple_journal"].unique())
 
 
-chargebacks_df = fraud_obj.filter_chargeback_records()
-#print(chargebacks_df)
-settled_df = fraud_obj.filter_settled_records()
+#filter the dataframe per simple_journal category
+#chargebacks_df = fraud_obj.filter_records("Chargeback")
+#settled_df = fraud_obj.filter_records("Settled")
+#refused_df = fraud_obj.filter_records("Refused")
 
-refused_df = fraud_obj.filter_refused_records()
 
-print(chargebacks_df.shape)
-
-print(settled_df.shape)
-
-print (refused_df.shape)
+#barplot with number of settled, chargebacks and refused transactions
+#fraud_obj.plot_data_balance()
 
 
 # NOTES
